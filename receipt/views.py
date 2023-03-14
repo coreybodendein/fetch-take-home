7814a63f397add7d015edfa6953b37e7037d03f5
Corey Bodendein <corey.bodendein@gmail.com>
2023-03-14 17:13:35 -0500

    Add the API
    This is the meat of the project.

--- receipt/views.py
+++ receipt/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Receipt
+from .serializers import ReceiptSerializer, ReceiptModelCreateSerialzier, ReceiptModelRetrieveSerialzier
 
-# Create your views here.
+
+class ReceiptProcessorGetView(APIView):
+    def get(self, request, _id):
+        try:
+            receipt = Receipt.objects.get(id=_id)
+            return Response(ReceiptModelRetrieveSerialzier(instance=receipt).data)
+        except Receipt.DoesNotExist:
+            return Response('No receipt found for that id', status=404)
+
+
+class ReceiptProcessorPostView(APIView):
+    def post(self, request):
+        serializer = ReceiptSerializer(data=request.data)
+        if serializer.is_valid():
+            receipt = serializer.save()
+            return Response(ReceiptModelCreateSerialzier(receipt).data)
+        else:
+            return Response('The receipt is invalid', status=400)
